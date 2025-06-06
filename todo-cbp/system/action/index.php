f2c7da88ec01a2a94a6dbd89d906a2a05e44da33
Yerenzter <yerenzter@yandex.com>
2025-06-06 20:30:42 +0800

    todo-cbp system

---
+++ todo-cbp/system/action/index.php
@@ -0,0 +1,33 @@
+<?php
+require("http://localhost/todo/db.php");
+
+$id = $_GET["id"];
+$isDone = $_GET["isdone"];
+$isDeleted = $_GET["isdeleted"];
+
+if ($isDone == "nofill") {
+    $take_action = $pdo->prepare("UPDATE tasks SET isDeleted=:deleted WHERE
+    id=:id");
+    $take_action->bindParam(":id", $id);
+    $take_action->bindParam(":deleted", $isDeleted);
+    $take_action->execute();
+    return;
+}
+
+if ($isDeleted == "nofill") {
+    $take_action = $pdo->prepare("UPDATE tasks SET isDone=:done WHERE id=:id");
+    $take_action->bindParam(":id", $id);
+    $take_action->bindParam(":done", $isDone);
+    $take_action->execute();
+    return;
+}
+
+if ($isDone != "nofill" and $isDeleted != "nofill") {
+    $take_action = $pdo->prepare("UPDATE tasks SET isDone=:done,
+    isDeleted=:deleted WHERE id=:id");
+    $take_action->bindParam(":id", $id); $take_action->bindParam(":done", $isDone);
+    $take_action->bindParam(":deleted", $isDeleted);
+    $take_action->execute();
+    return;
+}
+?>
